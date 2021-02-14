920e8125b18db20bc3c8fbf1628b3795e9ef74e0
zzztima <64201197+zzztima@users.noreply.github.com>
2021-02-14 18:18:47 +0300

    Add files via upload

---
+++ Fraction.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace Calc
+{
+    struct Fraction
+    {
+        //      numerator  
+        // Count-----------
+        //      denominator
+        public int numerator;
+        public int denominator;
+        public int count;
+
+        public static Fraction[] Equally(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction[] r = new Fraction[2];
+
+            r[0].denominator = fraction1.denominator * fraction2.denominator;
+            r[1].denominator = fraction1.denominator * fraction2.denominator;
+
+            r[0].count = fraction1.count;
+            r[1].count = fraction2.count;
+
+            r[0].numerator = fraction1.numerator * fraction2.denominator;
+            r[1].numerator = fraction2.numerator * fraction1.denominator;
+
+            return r;
+        }
+
+        public static Fraction operator +(Fraction fra1, Fraction fra2)
+        {
+            Fraction f1;
+            Fraction f2;
+
+            Fraction[] tmp = Equally(fra1, fra2);
+
+            f1 = tmp[0];
+            f2 = tmp[1];
+
+            Fraction r = f1;
+            r.numerator = f1.numerator + f2.numerator;
+            r.count = f1.count + f2.count;
+
+            return r;
+        }
+        
+        public static Fraction operator -(Fraction fra1, Fraction fra2)
+        {
+            Fraction f1;
+            Fraction f2;
+
+            Fraction[] tmp = Equally(fra1, fra2);
+
+            f1 = tmp[0];
+            f2 = tmp[1];
+
+            Fraction r = f1;
+            r.numerator = f1.numerator - f2.numerator;
+            r.count = f1.count - f2.count;
+
+            return r;
+        }
+        
+        public static Fraction operator *(Fraction fra1, Fraction fra2)
+        {
+            Fraction r = fra1;
+            r.numerator = fra1.numerator * fra2.numerator;
+            r.denominator = fra1.denominator * fra1.denominator;
+
+            return r;
+        }
+        
+        public static Fraction operator /(Fraction fra1, Fraction fra2)
+        {
+            Fraction r = fra1;
+            r.numerator = fra1.numerator * fra2.denominator;
+            r.denominator = fra1.denominator * fra1.numerator;
+
+            return r;
+        }
+
+        public override string ToString()
+        {
+            string[] r = new string[3];
+
+            int len = ((denominator.ToString().Length < numerator.ToString().Length) ? numerator.ToString().Length : denominator.ToString().Length) + 2;
+
+            r[0] = " " + numerator.ToString() + " ";
+            r[2] = " " + denominator.ToString() + " ";
+
+            for (int i = 0; i < len; i++)
+            {
+                r[1] += "-";
+            }
+
+            return r[0] + "\n" + r[1] + "\n" + r[2];
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Calc
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Fraction fraction1;
+        Fraction fraction2;
+        DispatcherTimer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            fraction1 = new Fraction();
+            fraction2 = new Fraction();
+            SetFraction();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Colc();
+        }
+
+        private void SetFraction()
+        {
+            try
+            {
+                this.fraction1.numerator = int.Parse(f1n.Text);
+                this.fraction1.denominator = int.Parse(f1d.Text);
+                this.fraction1.count = int.Parse(f1c.Text);
+
+                this.fraction2.numerator = int.Parse(f2n.Text);
+                this.fraction2.denominator = int.Parse(f2d.Text);
+                this.fraction2.count = int.Parse(f2c.Text);
+            }
+            catch
+            {
+                // TODO: Show error
+            }
+        }
+
+        private void Colc()
+        {
+            SetFraction();
+            Fraction fraction;
+            switch (Operator.SelectedIndex)
+            {
+                case 0:
+                    fraction = fraction1 + fraction2;
+                    break;
+                case 1:
+                    fraction = fraction1 - fraction2;
+                    break;
+                case 2:
+                    fraction = fraction1 * fraction2;
+                    break;
+                case 3:
+                    fraction = fraction1 / fraction2;
+                    break;
+                default:
+                    throw new Exception("Operator not found");
+            }
+
+            f3n.Text = fraction.numerator.ToString();
+            f3d.Text = fraction.denominator.ToString();
+            f3c.Text = fraction.count.ToString();
+        }
+    }
+}
